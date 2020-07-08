f1377e0213e6fd00df75615e7a817e15201dada0
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-08 11:58:30 -0300

    Config Wrapper

--- src/Screens/Main/index.js
+++ src/Screens/Main/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-import {StatusBar, Text} from 'react-native';
+import {StatusBar} from 'react-native';
+
+import {Wrapper} from './styles';
 
 export default function Main(){
     return(
         <>
             <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-            <Text>Aqui vai a tela Main</Text>
+
+            <Wrapper>
+
+                
+
+            </Wrapper>
+            
         </>
     )
 }
