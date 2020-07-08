27cd940b85d35ce47a0b56dacd080c7c112603b8
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-08 12:36:52 -0300

    Import Images in MainLanguages Components

--- src/Components/MainLanguages/index.js
+++ src/Components/MainLanguages/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import Javascript from '../../Images/Languages/Javascript.png';
+import PHP from '../../Images/Languages/PHP.png';
+import Python from '../../Images/Languages/Python.png';
+import CSharpe from '../../Images/Languages/CSharpe.png';
+import Java from '../../Images/Languages/Java.png';
+import C from '../../Images/Languages/C.png';
+import Ruby from '../../Images/Languages/Ruby.png';
+import Swift from '../../Images/Languages/Swift.png';
+import CPlus from '../../Images/Languages/CPlus.png';
+
 import {Title} from './styles';
 
 export default function MainLanguages(){
