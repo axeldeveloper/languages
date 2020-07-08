732a8167798d66096e81fade6551650079c2bfe7
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-08 11:29:22 -0300

    Finish Screen

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import Developer from '../../Images/HomeScreen/developer.png';
 
 import {Wrapper,
         Container,
         Title,
-        
+        Img,
+        ButtonInit,
+        TitleButton,
 } from './styles';
 
 export default function Home(){
@@ -16,7 +20,15 @@ export default function Home(){
 
                 <Title>Linguagens de Programação</Title>
 
-                
+                <Img source={Developer} alt="Desenvolvedor" />
+
+                <ButtonInit>
+
+                    <TitleButton>Iniciar</TitleButton>
+
+                    <MaterialCommunityIcons name="location-enter" size={24} color="black" />
+
+                </ButtonInit>
 
             </Container>
 
--- src/Screens/Home/styles.js
+++ src/Screens/Home/styles.js
@@ -2,19 +2,42 @@ import styled from 'styled-components/native';
 
 export const Wrapper = styled.SafeAreaView`
     flex:1;
-    background-color:#3F2047;
+    background-color:#281336;
     align-items:center;
     justify-content:center;
 `;
 
 export const Container = styled.View`
-    background-color:#ff1493;
+    align-items:center;
+    justify-content:center;
+    padding-top:80px;
 `;
 
 export const Title = styled.Text`
     color:#fff;
     font-weight:bold;
-    font-size:18px;
+    font-size:23px;
+    margin-bottom:80px;
 `;
 
+export const Img = styled.Image`
+    width:250px;
+    height:250px;
+`;
 
+export const ButtonInit = styled.TouchableOpacity`
+    background-color:#c5e3f6;
+    margin-top:120px;
+    width:300px;
+    padding:15px 0;
+    border-radius:10px;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const TitleButton = styled.Text`
+    font-weight:bold;
+    font-size:16px;
+    margin-right:10px;
+`;
